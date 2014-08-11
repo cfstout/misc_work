1c6d187fd57e8f7987ee8cbe1d6bb30ee7950c45
Clayton F Stout <Clayton.Stout@bazaarvoice.com>
2014-08-11 11:03:29 -0500

    finished command line options

--- ES_Log_parser.py
+++ ES_Log_parser.py
@@ -1,3 +1,7 @@
+
+
+##!/usr/bin/env python
+
 orig_logs = open('/Users/clayton.stout/BazaarVoice/SQLDebug.txt','r')
 metrics = open('/Users/clayton.stout/BazaarVoice/metrics1.txt', 'w')
 tables = open('/Users/clayton.stout/BazaarVoice/tables1.txt','w')
@@ -23,3 +27,23 @@ for table in sorted(metrics_list, key=metrics_list.get, reverse=True):
 
 metrics.close()
 tables.close()
+
+def main():
+    p = argparse.ArgumentParser(description = 'Tool to take in log4j out file with ES Debug logging and parse/classify the JSON queries',
+                                formatter_class = argparse.RawTextHelpFormatter,
+                                epilog='Usage examples:\n\n' \
+                                       'To interactively filter the logs in catalina.out:\n' \
+                                       '   ./ES_Log_parser -i catalina.out \n\n' \
+                                       'To filter by jcpenney tables queried in the logs from file /User/username/gitwork/logs/logfile.txt:\n' \
+                                       '   ./ES_Log_parser -t jcpenney /User/username/gitwork/logs/logfile.txt \n\n' \
+                                       'To see all queries to a catalog table from log file catalina.out: \n' \
+                                       '   ./ES_Log_parser -t catalog -l')
+
+    p.add_argument('-i', '--interactive', dest='interactive', default='false', choices=['true','false'],
+                   help='Interactive Mode. Default is %(default)s')
+    p.add_argument('-t', '--table', dest='table', default='',
+                   help='Table filter param. Default is %(default)s')
+    p.add_argument('-l', '--list', dest='clusters', default='false', choices=['true','false'],
+                   help='List the queries for the provided filter. Default is: do not list')
+
+    options = p.parse_args()
